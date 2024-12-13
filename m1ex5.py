06b2613370b4652adc48f7669f4f5c36d68e615a
hemor0i <screppyy0@gmail.com>
2024-12-13 22:29:59 +0300

    Неизменяемые и изменяемые объекты. Кортежи

---
+++ m1ex5.py
@@ -0,0 +1,10 @@
+immutable_var = 12, True, 'Leonid'
+print(immutable_var)
+# immutable_var[1] = False
+# print(immutable_var)
+# Потому что это кортеж
+
+mutable_list = [121 , False, 'Andrey']
+mutable_list[2] = 'Gena'
+print (mutable_list)
+
