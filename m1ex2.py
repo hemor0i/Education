404307024181ebcd1e9f0e4bc707a0e7c7a95c90
hemor0i <screppyy0@gmail.com>
2024-12-15 15:27:15 +0300

    Переменные

--- m1ex2.py
+++ m1ex2.py
@@ -1,4 +1,5 @@
 completed_homework = 12
 count_hours_spent = 1.5
 course = 'Python'
-print(count_hours_spent / completed_homework)
+time_for_1_task =  count_hours_spent / completed_homework
+print(f'{course}, всего задач: {completed_homework}, затраченное время:  {count_hours_spent}, среднее время выполнения {time_for_1_task} часа')
