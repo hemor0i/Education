a7ea4f58ba64b9115b42b49d9e7d7dfbd68fa772
hemor0i <screppyy0@gmail.com>
2024-12-25 19:57:19 +0300

    Функции в Python. Функция с параметром

--- m2ex4.py
+++ m2ex4.py
@@ -2,11 +2,14 @@
 def get_matrix(n, m, value):
     matrix = []
     for i in range(n):
-        matrix.append([])
+        matrix.append([])                               # Добавляем "строку" в список
         for j in range(m):
-            matrix[i].append(value)
+            matrix[i].append(value)                     # Заполняем список значениями
     return matrix
 
 
-matrix = get_matrix(4, 2, 13)
+n = int(input('Введите количество строк матрицы: '))    # Определяем как целочисленное, тк input всегда типа str
+m = int(input('Введите количество столбцов матрицы: ')) # То же самое
+value = input('Введите значение заполнения матрицы: ')  # Чтобы можно было любыми символами заполнять
+matrix = get_matrix(n, m, value)
 print(matrix)
