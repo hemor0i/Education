abb463e3575d50c28d716048d8f7e4909385d60a
hemor0i <screppyy0@gmail.com>
2025-01-06 18:01:32 +0300

    Произвольное число параметров

--- m3ex4.py
+++ m3ex4.py
@@ -2,10 +2,10 @@
 def single_root_words(root_word, *other_words):
     same_words = []
     for i in range(len(other_words)):
+        # Сравним совпадения слов
         if root_word.lower() in other_words[i].lower() or other_words[i].lower() in root_word.lower():
-            same_words.append((other_words[i]))
-    return same_words
-
+            same_words.append((other_words[i])) # Добавим в список однокоренные слова
+    return same_words   # Выведем список
 
 
 result1 = single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies')
